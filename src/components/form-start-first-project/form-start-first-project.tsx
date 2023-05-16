049b2661f2a689a9c60929dc45ed2a9978d12f84
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-05-16 13:48:59 +0300

    Added default values to first form

--- src/components/form-start-first-project/form-start-first-project.tsx
+++ src/components/form-start-first-project/form-start-first-project.tsx
@@ -16,6 +16,10 @@ function FormStartFirstProject() {
     'Tools',
   ]
 
+  const defaultName = store.getState().form.formStepsData.startFirstProject.name || ''
+  const defaultUrl = store.getState().form.formStepsData.startFirstProject.url || ''
+  const defaultCategory = store.getState().form.formStepsData.startFirstProject.category || []
+
   async function handleSubmit(formData: FormData) {
     'use server'
     const formattedData: IStartFirstProject = {
@@ -39,7 +43,13 @@ function FormStartFirstProject() {
       <form action={handleSubmit}>
         <div>
           <label htmlFor='name'>Project Name (It can be changed later)</label>
-          <input id='name' type='text' name='name' placeholder='Project Name' />
+          <input
+            id='name'
+            type='text'
+            name='name'
+            defaultValue={defaultName}
+            placeholder='Project Name'
+          />
         </div>
 
         <div>
@@ -48,6 +58,7 @@ function FormStartFirstProject() {
             id='url'
             type='text'
             name='url'
+            defaultValue={defaultUrl}
             placeholder='Project URL'
             prefix='Alphaguilty.io/'
           />
@@ -57,7 +68,13 @@ function FormStartFirstProject() {
           <label>Project Category (It cannot be changed after creation)</label>
           {categories.map((category) => (
             <div key={category}>
-              <input id={category} type='checkbox' name='category' value={category} />
+              <input
+                id={category}
+                type='checkbox'
+                name='category'
+                value={category}
+                defaultChecked={defaultCategory.includes(category)}
+              />
               <label htmlFor={category}>{category}</label>
             </div>
           ))}
