543e829f2797e8719599e989bbc22c5d2cdd9485
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-05-18 21:52:23 +0300

    Changed project list page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+
+import { IProject } from '@/store/formSlice'
+
+async function getProjects() {
+  const data = await fetch('http://localhost:3000/api/projects')
+
+  return data.json() as Promise<{ projects: IProject[] }>
+}
+
+export default async function Page() {
+  const { projects } = await getProjects()
+
+  return (
+    <>
+      <section>
+        <div>
+          <Link href={'/'}>New project</Link>
+        </div>
+
+        <ul style={{ margin: '20px 0 0 0' }}>
+          {projects.map(
+            ({ id, name, url, category, mainGoal, workersAmount, launchType, email }) => (
+              <>
+                <li key={id} style={{ margin: '0 0 0 20px' }}>
+                  <h3>{name}</h3>
+                  <p>{url}</p>
+                  <p>{category.join(', ')}</p>
+                  <p>{mainGoal}</p>
+                  <p>{workersAmount}</p>
+                  <p>{launchType}</p>
+                  <p>{email}</p>
+                </li>
+                <hr style={{ margin: '20px 0 0 0' }} />
+              </>
+            ),
+          )}
+        </ul>
+      </section>
+    </>
+  )
+}
