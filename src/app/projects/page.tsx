cb2057682ade9ff0b74f91f9d13ad4117668018f
Oleksii Martynenko <oleksii.martynenko.official@gmail.com>
2023-05-19 13:22:07 +0300

    Try fix json of fetch at projects page, try short url

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,27 +5,27 @@ import { IProject } from '@/store/formSlice'
 
 async function getProjects() {
   try {
-    const response = await fetch('https://dummyjson.com/posts?limit=3')
-    // const response = await fetch(process.env.API_URL + '/projects')
+    // const response = await fetch('https://dummyjson.com/posts?limit=3')
+    const response = await fetch('api/projects')
     const responseClone = response.clone()
     const dataText = await responseClone.text()
     console.log('🚀 ~ getProjects ~ dataText:', dataText)
-    return response.json() as Promise<{
-      posts: {
-        id: number
-        title: string
-      }[]
-    }>
-    // return response.json() as Promise<{ projects: IProject[] }>
+    // return response.json() as Promise<{
+    //   posts: {
+    //     id: number
+    //     title: string
+    //   }[]
+    // }>
+    return response.json() as Promise<{ projects: IProject[] }>
   } catch (error) {
     console.log('error:', error)
-    // return { projects: [] }
-    return { posts: [] }
+    return { projects: [] }
+    // return { posts: [] }
   }
 }
 
 export default async function Page() {
-  const projects = await getProjects()
+  const { projects } = await getProjects()
   console.log('🚀 ~ Page ~ projects:', projects)
 
   return (
@@ -36,15 +36,15 @@ export default async function Page() {
         </div>
 
         <ul style={{ margin: '20px 0 0 0' }}>
-          {projects.posts.map(({ id, title }) => (
+          {/* {projects.map(({ id, title }) => (
             <>
               <li key={id} style={{ margin: '0 0 0 20px' }}>
                 <h3>{title}</h3>
               </li>
               <hr style={{ margin: '20px 0 0 0' }} />
             </>
-          ))}
-          {/* {projects.map(
+          ))} */}
+          {projects.map(
             ({ id, name, url, category, mainGoal, workersAmount, launchType, email }) => (
               <>
                 <li key={id} style={{ margin: '0 0 0 20px' }}>
@@ -59,7 +59,7 @@ export default async function Page() {
                 <hr style={{ margin: '20px 0 0 0' }} />
               </>
             ),
-          )} */}
+          )}
         </ul>
       </section>
     </>
